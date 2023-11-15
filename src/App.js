c0e0cfdac6f28c039d528817a0839f0e8b7e6a44
MManimekalai <manimekalai.eee123@gmail.com>
2023-11-15 20:27:43 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -9,41 +9,21 @@ import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
-    <>
+
     <Router>
       <div id="wrapper">
         <NavBar />
         <Routes>
           <Route path="/home" element={<Home />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ContextComponent>
-                <Dashboard />
-              </ContextComponent>
-            }
-          />
-          <Route
-            path="/bookslist"
-            element={
-              <ContextComponent>
-                <BooksList />
-              </ContextComponent>
-            }
-          />
-          <Route
-            path="/addbook"
-            element={
-              <ContextComponent>
-                <AddBook />
-              </ContextComponent>
-            }
-          />
+          <Route path="/dashboard" element={<ContextComponent><Dashboard /></ContextComponent>} />
+          <Route path="/bookslist" element={<ContextComponent><BooksList /></ContextComponent>} />
+          <Route path="/addbook" element={<ContextComponent><AddBook /></ContextComponent>} />
           <Route path="/*" element={<Navigate to="/home" />} />
         </Routes>
+
       </div>
     </Router>
-    </>
+
   );
 }
 
